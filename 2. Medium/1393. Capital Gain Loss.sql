bb0f0eaefe0c77bd9caffbe2e276f83450abd2fa
dnkz <dan.misc.101@gmail.com>
2023-12-17 02:10:10 -0500

    completed all available free medium sql problems

---
+++ 2. Medium/1393. Capital Gain Loss.sql
@@ -0,0 +1,60 @@
+-- Table: Stocks
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | stock_name    | varchar |
+-- | operation     | enum    |
+-- | operation_day | int     |
+-- | price         | int     |
+-- +---------------+---------+
+-- (stock_name, operation_day) is the primary key (combination of columns with unique values) for this table.
+-- The operation column is an ENUM (category) of type ('Sell', 'Buy')
+-- Each row of this table indicates that the stock which has stock_name had an operation on the day operation_day with the price.
+-- It is guaranteed that each 'Sell' operation for a stock has a corresponding 'Buy' operation in a previous day. It is also guaranteed that each 'Buy' operation for a stock has a corresponding 'Sell' operation in an upcoming day.
+ 
+
+-- Write a solution to report the Capital gain/loss for each stock.
+
+-- The Capital gain/loss of a stock is the total gain or loss after buying and selling the stock one or many times.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Stocks table:
+-- +---------------+-----------+---------------+--------+
+-- | stock_name    | operation | operation_day | price  |
+-- +---------------+-----------+---------------+--------+
+-- | Leetcode      | Buy       | 1             | 1000   |
+-- | Corona Masks  | Buy       | 2             | 10     |
+-- | Leetcode      | Sell      | 5             | 9000   |
+-- | Handbags      | Buy       | 17            | 30000  |
+-- | Corona Masks  | Sell      | 3             | 1010   |
+-- | Corona Masks  | Buy       | 4             | 1000   |
+-- | Corona Masks  | Sell      | 5             | 500    |
+-- | Corona Masks  | Buy       | 6             | 1000   |
+-- | Handbags      | Sell      | 29            | 7000   |
+-- | Corona Masks  | Sell      | 10            | 10000  |
+-- +---------------+-----------+---------------+--------+
+-- Output: 
+-- +---------------+-------------------+
+-- | stock_name    | capital_gain_loss |
+-- +---------------+-------------------+
+-- | Corona Masks  | 9500              |
+-- | Leetcode      | 8000              |
+-- | Handbags      | -23000            |
+-- +---------------+-------------------+
+
+SELECT
+    stock_name,
+    SUM(CASE WHEN operation = 'Buy' THEN -price ELSE price END) AS capital_gain_loss
+FROM
+    Stocks
+GROUP BY
+    stock_name
---
+++ 2. Medium/608. Tree Node.sql
@@ -0,0 +1,55 @@
+-- Table: Tree
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | p_id        | int  |
+-- +-------------+------+
+-- id is the column with unique values for this table.
+-- Each row of this table contains information about the id of a node and the id of its parent node in a tree.
+-- The given structure is always a valid tree.
+ 
+
+-- Each node in the tree can be one of three types:
+
+-- "Leaf": if the node is a leaf node.
+-- "Root": if the node is the root of the tree.
+-- "Inner": If the node is neither a leaf node nor a root node.
+-- Write a solution to report the type of each node in the tree.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+-- Input: 
+-- Tree table:
+-- +----+------+
+-- | id | p_id |
+-- +----+------+
+-- | 1  | null |
+-- | 2  | 1    |
+-- | 3  | 1    |
+-- | 4  | 2    |
+-- | 5  | 2    |
+-- +----+------+
+-- Output: 
+-- +----+-------+
+-- | id | type  |
+-- +----+-------+
+-- | 1  | Root  |
+-- | 2  | Inner |
+-- | 3  | Leaf  |
+-- | 4  | Leaf  |
+-- | 5  | Leaf  |
+-- +----+-------+
+
+SELECT
+    id,
+    CASE
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT DISTINCT p_id FROM Tree) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM
+    Tree
